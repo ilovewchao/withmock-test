64429116e8558d82a5957c4b74c679e135db069f
ilovewchao <wcpzju@163.com>
2014-07-15 20:18:23 +0800

    Update foo_test.go

--- fail2/foo/foo_test.go
+++ fail2/foo/foo_test.go
@@ -5,21 +5,21 @@ import (
     "code.google.com/p/gomock/gomock"
 	
     "github.com/wchao/example/baz" //mock
-	"encoding/json" //mock
+    "encoding/json" //mock
 )
 
 func TestTryMe(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
 
-	baz.MOCK().SetController(ctrl)
-	json.MOCK().SetController(ctrl)
+    baz.MOCK().SetController(ctrl)
+    json.MOCK().SetController(ctrl)
 	
-	fo := &Foo{}
-	bz := &baz.Baz{}
+    fo := &Foo{}
+    bz := &baz.Baz{}
 	
-	json.EXPECT().Unmarshal(gomock.Any(), bz).Return(nil)
-	bz.EXPECT().DoSomething().Return(nil)
+    json.EXPECT().Unmarshal(gomock.Any(), bz).Return(nil)
+    bz.EXPECT().DoSomething().Return(nil)
 	
-	fo.TryMe()
+    fo.TryMe()
 }
