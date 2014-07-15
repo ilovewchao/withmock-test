ce7675488d29345448c5b517428e8839d22c1cb1
ilovewchao <wcpzju@163.com>
2014-07-15 20:17:09 +0800

    Update baz.go

--- fail2/baz/baz.go
+++ fail2/baz/baz.go
@@ -1,16 +1,16 @@
 package baz
 
 import (
-	"fmt"
+    "fmt"
 )
 
 type Baz struct{
-	name string
+    name string
 }
 
 func (b *Baz) DoSomething() error {
-	if len(b.name) == 0 {
-		return fmt.Errorf("has no name")
-	}
-	return nil
+    if len(b.name) == 0 {
+    	return fmt.Errorf("has no name")
+    }
+    return nil
 }
